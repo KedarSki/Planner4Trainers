365324e227f29797d073902b33008eea2b66c865
unknown <radoslaw.zamojski@gmail.com>
2022-12-26 18:04:13 +0100

    Commit on new computer to get some files ready to use

--- Planner4Trainers/Client/Program.cs
+++ Planner4Trainers/Client/Program.cs
@@ -33,7 +33,4 @@ builder.Services.AddDateRangePicker(config =>
     config.ApplyLabel = "My Apply Label";
 });
 
-Syncfusion.Licensing.SyncfusionLicenseProvider
-    .RegisterLicense("NzE0MjgyQDMyMzAyZTMyMmUzMEtacWtSVmRwc2xKNS9ValM4SHNrK3AwTUtzOG9Tb0lNUlowVFllYWJMZWs9");
-
 await builder.Build().RunAsync();
